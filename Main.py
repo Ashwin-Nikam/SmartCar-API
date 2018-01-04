2330c9647920a5069d970404cd5afc33d2ec07e5
Ashwin Nikam <anikam@buffalo.edu>
2018-01-04 18:27:01 -0500

    SmartCar API for vehicle info done

--- Main.py
+++ Main.py
@@ -14,7 +14,7 @@ def gm_api_post(service):
         user_input = input("Do you want to start or stop the engine? [START/STOP]")
         if user_input == 'START':
             payload = {'id': '1234', 'command': "START_VEHICLE",
-                   'responseType': 'JSON'}
+                       'responseType': 'JSON'}
         elif user_input == 'STOP':
             payload = {'id': '1234', 'command': "STOP_VEHICLE",
                        'responseType': 'JSON'}
@@ -27,10 +27,33 @@ def gm_api_post(service):
     return response
 
 
-print(gm_api_post(vehicle_info))
-print()
-print(gm_api_post(security))
-print()
-print(gm_api_post(energy_level))
-print()
-print(gm_api_post(engine))
+def test_gm_api():
+    print(gm_api_post(vehicle_info))
+    print()
+    print(gm_api_post(security))
+    print()
+    print(gm_api_post(energy_level))
+    print()
+    print(gm_api_post(engine))
+
+
+def parse_vehicle_info():
+    info = {}
+    vehicle_info_json = gm_api_post(vehicle_info)
+    data = vehicle_info_json['data']
+    color = data['color']['value']
+    info["color"] = color
+    drive_train = data['driveTrain']['value']
+    info["driveTrain"] = drive_train
+    vin = data['vin']['value']
+    info["vin"] = vin
+    if data['fourDoorSedan']['value'] == 'True':
+        door_count = 4
+    elif data['twoDoorCoupe']['value'] == 'True':
+        door_count = 2
+    else:
+        door_count = None
+    info["doorCount"] = door_count
+    return info
+
+print(parse_vehicle_info())
