4b2a8ca773d1d7b6c1f514dce28828d9a32a81c7
Ashwin Nikam <anikam@buffalo.edu>
2018-01-04 01:48:18 -0500

    Engine not working

--- Main.py
+++ Main.py
@@ -2,9 +2,26 @@ import requests
 import json
 
 BASE_URL = 'http://gmapi.azurewebsites.net'
+vehicle_info = "/getVehicleInfoService"
+security = "/getSecurityStatusService"
+energy_level = "/getEnergyService"
+engine = "/actionEngineService"
 
-request_url = BASE_URL + "/getVehicleInfoService"
-payload = {'id': '1234', 'responseType': 'JSON'}
-headers = {'Content-Type': 'application/json'}
-user_info = requests.post(request_url, headers=headers, data=json.dumps(payload)).json()
-print(user_info)
+
+def gm_api_post(service):
+    request_url = BASE_URL + service
+    if service == engine:
+        payload = {'id': '1234', 'command': "START_VEHICLE|STOP_VEHICLE",
+                   'responseType': 'JSON'}
+    else:
+        payload = {'id': '1234', 'responseType': 'JSON'}
+    headers = {'Content-Type': 'application/json'}
+    response = requests.post(request_url, headers=headers, data=json.dumps(payload)).json()
+    return response
+
+
+print(gm_api_post(vehicle_info))
+print()
+print(gm_api_post(security))
+print()
+print(gm_api_post(energy_level))
