94411dee5ea787b034a11f3bc6dcd611dfe63396
Ashwin Nikam <anikam@buffalo.edu>
2018-01-05 17:35:04 -0500

    Updated models

--- smartcar/webapp/models.py
+++ smartcar/webapp/models.py
@@ -8,20 +8,25 @@ class Vehicle(models.Model):
     color = models.CharField(max_length=50)
     door_count = models.IntegerField()
     drive_train = models.CharField(max_length=50)
+    main_id = models.CharField(primary_key=True, max_length=50)
 
 
 class Security(models.Model):
+    vehicle = models.ForeignKey(Vehicle)
     location = models.CharField(max_length=50)
     locked = models.BooleanField()
 
 
 class Fuel(models.Model):
+    vehicle = models.ForeignKey(Vehicle)
     percent = models.FloatField()
 
 
 class Battery(models.Model):
+    vehicle = models.ForeignKey(Vehicle)
     percent = models.FloatField()
 
 
 class Engine(models.Model):
+    vehicle = models.ForeignKey(Vehicle)
     status = models.CharField(max_length=50)
