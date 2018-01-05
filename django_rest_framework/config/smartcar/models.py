9a526a102fb3c14ca3bd14463022c49b5a336c26
Ashwin Nikam <anikam@buffalo.edu>
2018-01-05 15:35:44 -0500

    Writing first API

--- django_rest_framework/config/smartcar/models.py
+++ django_rest_framework/config/smartcar/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 
-class Vehicles(models.Model):
+class Vehicle(models.Model):
     vin = models.IntegerField()
     color = models.CharField(max_length=50)
     door_count = models.IntegerField()
