36ac0ad9116c2b1d202a7062e3cc75007e73e988
Ashwin Nikam <anikam@buffalo.edu>
2018-01-05 18:24:29 -0500

    Getting responses on console

--- smartcar/webapp/urls.py
+++ smartcar/webapp/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^vehicles/\d{4}', views.vehicle_info, name='index')
-
+    url(r'^vehicles/\d{4}$', views.vehicle_info),
+    url(r'^vehicles/\d{4}/doors', views.security),
+    url(r'^vehicles/\d{4}/fuel', views.fuel),
+    url(r'^vehicles/\d{4}/battery', views.battery)
 ]
--- smartcar/webapp/views.py
+++ smartcar/webapp/views.py
@@ -1,10 +1,25 @@
 from django.http import HttpResponse
-import GeneralMotors as gm
 import SmartCar as sc
+
 # Create your views here.
 
 
 def vehicle_info(request):
     print(type(request))
     print(sc.parse_vehicle_info(1234))
-    return HttpResponse("<H2> Hello world </H2>")
+    return HttpResponse("<H2> Vehicle info </H2>")
+
+
+def security(request):
+    print(sc.parse_security_info(1234))
+    return HttpResponse("<H2> Security </H2>")
+
+
+def fuel(request):
+    print(sc.parse_fuel_info(1234))
+    return HttpResponse("<H2> Fuel </H2>")
+
+
+def battery(request):
+    print(sc.parse_battery_info(1234))
+    return HttpResponse("<H2> Battery </H2>")
