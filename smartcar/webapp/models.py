945843093d7a8533d1cbda2192471fb1340bd055
Ashwin Nikam <anikam@buffalo.edu>
2018-01-05 16:40:04 -0500

    Defined all the models

--- smartcar/webapp/models.py
+++ smartcar/webapp/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Vehicle(models.Model):
+    vin = models.CharField(max_length=50)
+    color = models.CharField(max_length=50)
+    door_count = models.IntegerField()
+    drive_train = models.CharField(max_length=50)
+
+
+class Security(models.Model):
+    location = models.CharField(max_length=50)
+    locked = models.BooleanField()
+
+
+class Fuel(models.Model):
+    percent = models.FloatField()
+
+
+class Battery(models.Model):
+    percent = models.FloatField()
+
+
+class Engine(models.Model):
+    status = models.CharField(max_length=50)
