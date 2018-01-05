177356d3b37e13749974e9a6a799df7e826d5a89
Ashwin Nikam <anikam@buffalo.edu>
2018-01-05 15:56:59 -0500

    Starting brand new

--- Main.py
+++
@@ -1,7 +0,0 @@
-import SmartCar as sc
-
-
-# POST /vehicles/1234/engine Content-Type: application/json { "action": "START" }
-
-request_url = input()
-sc.api_call(request_url)
--- django_rest_framework/config/smartcar/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class SmartcarConfig(AppConfig):
-    name = 'smartcar'
