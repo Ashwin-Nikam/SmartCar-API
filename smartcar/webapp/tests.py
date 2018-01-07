31af6a1b95c2d2f83f386b5b16736b3d69521706
Ashwin Nikam <anikam@buffalo.edu>
2018-01-07 15:52:04 -0500

    Wrote first testcase

--- smartcar/webapp/tests.py
+++ smartcar/webapp/tests.py
@@ -1,10 +1,11 @@
 from django.test import TestCase
+import SmartCar as sc
+import GeneralMotors as gm
 
 # Create your tests here.
 
 
 class TestBasic(TestCase):
-    "Basic tests"
     def test_basic(self):
         a = 1
         self.assertEqual(1, a)
@@ -12,3 +13,19 @@ class TestBasic(TestCase):
     def test_basic_2(self):
         a = 1
         assert a == 1
+
+
+class GeneralMotorsApiTest(TestCase):
+    ids = [1234, 1235]
+
+    def test_vehicle_info(self):
+        service = sc.vehicle_info
+        for id in GeneralMotorsApiTest.ids:
+            response = gm.post_request(service, id, None, None)
+            self.assertEquals(response['status'], '200')
+
+    def test_vehicle_info(self):
+        service = sc.vehicle_info
+        for id in GeneralMotorsApiTest.ids:
+            response = gm.post_request(service, id, None, None)
+            self.assertEquals(response['status'], '200')
