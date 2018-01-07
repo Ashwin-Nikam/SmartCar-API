547674716f89237dbddda791c5534a627bfe1e8c
Ashwin Nikam <anikam@buffalo.edu>
2018-01-06 23:41:38 -0500

    Added git ignore

--- smartcar/smartcar/settings.py
+++ smartcar/smartcar/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'w7#3q_+sxl+-zh^)c2o)4_f6ft2t219s#aqij#2@mw%g)9oxwg'
+SECRET_KEY = config.secret_key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
