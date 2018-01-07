820883de857d4b253fd028661ef8c901e18c09ff
Ashwin Nikam <anikam@buffalo.edu>
2018-01-06 23:35:52 -0500

    Switch to master

--- myproject/webapp/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from . models import employees
-
-
-class employeeSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = employees
-        fields = '__all__'
