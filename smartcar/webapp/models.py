dbd84e03adecf5208dc60fcbb9a2102d79b341fe
Ashwin Nikam <anikam@buffalo.edu>
2018-01-07 03:02:36 -0500

    Db test successful

--- smartcar/webapp/models.py
+++ smartcar/webapp/models.py
@@ -4,24 +4,24 @@ from django.db import models
 
 
 class Vehicle(models.Model):
-    vin = models.CharField(max_length=50)
-    color = models.CharField(max_length=50)
-    door_count = models.IntegerField()
-    drive_train = models.CharField(max_length=50)
+    vin = models.CharField(max_length=50, default="")
+    color = models.CharField(max_length=50, default="")
+    door_count = models.IntegerField(default=1)
+    drive_train = models.CharField(max_length=50, default="")
 
 
 class Security(models.Model):
-    location = models.CharField(max_length=50)
-    locked = models.BooleanField()
+    location = models.CharField(max_length=50, default="")
+    locked = models.BooleanField(default=True)
 
 
 class Fuel(models.Model):
-    percent = models.CharField(max_length=50)
+    percent = models.CharField(max_length=50, default="")
 
 
 class Battery(models.Model):
-    percent = models.CharField(max_length=50)
+    percent = models.CharField(max_length=50, default="")
 
 
 class Engine(models.Model):
-    status = models.CharField(max_length=50)
+    status = models.CharField(max_length=50, default="")
