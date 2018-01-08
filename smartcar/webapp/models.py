22bb4da0ba1a4849ddfc33a4fb7b401103a3165c
Ashwin Nikam <anikam@buffalo.edu>
2018-01-07 19:11:52 -0500

    Linked key is model

--- smartcar/webapp/models.py
+++ smartcar/webapp/models.py
@@ -4,24 +4,28 @@ from django.db import models
 
 
 class Vehicle(models.Model):
-    vin = models.CharField(max_length=50, default="")
+    vin = models.CharField(max_length=50, default="", primary_key=True)
     color = models.CharField(max_length=50, default="")
     doorCount = models.IntegerField(default=1)
     driveTrain = models.CharField(max_length=50, default="")
 
 
 class Security(models.Model):
+    vehicle = models.ForeignKey(Vehicle, on_delete=models.CASCADE, default="")
     location = models.CharField(max_length=50, default="")
     locked = models.BooleanField(default=True)
 
 
 class Fuel(models.Model):
+    vehicle = models.ForeignKey(Vehicle, on_delete=models.CASCADE, default="")
     percent = models.CharField(max_length=50, default="")
 
 
 class Battery(models.Model):
+    vehicle = models.ForeignKey(Vehicle, on_delete=models.CASCADE, default="")
     percent = models.CharField(max_length=50, default="")
 
 
 class Engine(models.Model):
+    vehicle = models.ForeignKey(Vehicle, on_delete=models.CASCADE, default="")
     status = models.CharField(max_length=50, default="")
